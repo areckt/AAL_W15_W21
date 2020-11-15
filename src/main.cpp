576856faf93e841788a938af149d8814938e2585
areckt <arkadiusz.dawid.99@gmail.com>
2020-11-15 15:48:54 +0100

    Pobieranie rozmiaru tablicy z parametru wywolania programu

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,18 @@
  */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[]){
 
+    if(argc > 1){
+        int tableSize = stoi(argv[1], nullptr, 10);
+        cout << "Rozmiar tablicy to:" << tableSize << endl;
+    }
     cout << "Hello World" << endl;
+
 
     return 0;
 }
