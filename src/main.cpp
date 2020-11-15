bdb965d20a35904ed55f4ea0e9aa46b8db59189a
areckt <arkadiusz.dawid.99@gmail.com>
2020-11-15 15:18:43 +0100

    Created repository

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Arkadiusz Dawid,     300199
+ * Illia Yatskevich,    302211
+ * Projekt AAL W15 W21 - Tablica mieszajaca
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    cout << "Hello World" << endl;
+
+    return 0;
+}
