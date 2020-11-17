47fdd0e0ebd958e1e97c42160ffb8f6f2af852df
areckt <arkadiusz.dawid.99@gmail.com>
2020-11-17 01:34:20 +0100

    pierwszy zarys programu

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,26 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include "../include/hash.h"
 
 using namespace std;
 
-const int TableSize = 10;
-int hashFunction(string s);
+#define PRIME 7
+
+long int hashFunction(string s){        // oblicza hash, zwraca k (jeszcze przed modulo TableSize)
+    int g = 31;
+    long int k = 0;
+    for(char i : s){
+        k = g * k + (int)i;
+    }
+
+    return k;
+}
+string generate(void);                  // generuje losowe słowa, zwraca s
+int insertToHashTable(string s, int k); // wstawia słowo s do tablicy na pozycji k
+
+
 
 int main(int argc, char *argv[]){
 
@@ -26,6 +41,7 @@ int main(int argc, char *argv[]){
     }
     */
 
+    /*
     string HashTable[TableSize];
     string sTemp;
     int index;
@@ -42,16 +58,47 @@ int main(int argc, char *argv[]){
     for(int i = 0; i < TableSize; i++){
         cout << "Na pozycji " << i << ": " << HashTable[i] << endl;
     }
+    */
+    int TableSize = 11;
+
+    vector<string> hashTable (TableSize);
+    string s;
+    long int k;
+    int k1;
+
+    for(int i = 0; i < TableSize; i++){
+        s = generate();
+        k = hashFunction(s);
+        k1 = (int)(k%TableSize);
+
+        insertToHashTable(s, k1);
+        if(hashTable.at(k).length() != 0){  // if collision occurs
+            int k2 = PRIME - (k % PRIME);
+            int new_k;
+            for(int j = 1; ; j++){
+                new_k = (k1 + j*k2) % TableSize;
+
+                //check for collision
+                if(hashTable.at(new_k).length() == 0){
+                    insertToHashTable(s, new_k);
+                    break;
+                }
+            }
+        }
+        else{   // if no collision occurs
+            insertToHashTable(s, k1);
+        }
+    }
+
+    for(const auto& i : hashTable){
+        if(i.length() == 0) cout << "[-]" << endl;
+    }
 
     return 0;
 }
 
-int hashFunction(string s){
-    int g = 31;
-    long int k = 0;
-    for(char i : s){
-        k = g * k + (int)i;
-    }
-    k %= TableSize;
-    return (int)k;
+
+
+int insertToHashTable(string s, int k){
+    ;
 }
