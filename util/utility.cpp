42b60a61fc92270efc5e4a9916bc8aa400a0a345
areckt <arkadiusz.dawid.99@gmail.com>
2021-01-03 21:05:22 +0100

    Program pomocniczy

---
+++ util/utility.cpp
@@ -0,0 +1,80 @@
+/*
+ * Arkadiusz Dawid,     300199
+ * Illia Yatskevich,    302211
+ * Projekt AAL W15 W21 - Tablica mieszajaca
+ */
+
+/*
+    KOMPILACJA:     g++ utility.cpp -o prog -licuuc -licuio -licui18n
+    URUCHOMIENIE:   ./prog < in.txt > out.txt
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unicode/utypes.h>
+#include <unicode/unistr.h>
+#include <unicode/translit.h>
+
+using namespace std;
+
+string desaxUTF8(const string& str) {
+    // UTF-8 string -> UTF-16 UnicodeString
+    UnicodeString source = UnicodeString::fromUTF8(StringPiece(str));
+
+    // Transliterate UTF-16 UnicodeString
+    UErrorCode status = U_ZERO_ERROR;
+    Transliterator *accentsConverter = Transliterator::createInstance(
+            "NFD; [:M:] Remove; NFC", UTRANS_FORWARD, status);
+    accentsConverter->transliterate(source);
+
+    // UTF-16 UnicodeString -> UTF-8 std::string
+    std::string result;
+    source.toUTF8String(result);
+
+    return result;
+}
+
+string toLowerCase(string s)
+{
+    string result = s;
+    for (int i = 0; i < result.size(); i++)
+        if (result[i] >= 'A' && result[i] <= 'Z')
+            result[i] = tolower(result[i]);
+    return result;
+}
+
+string removePunctuation(string s){
+    string result = "";
+    for(int i = 0; i < s.length(); ++i){
+        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')){
+            result += s[i];
+        }
+    }
+    return result;
+}
+
+int main(){
+
+    vector<string> text;
+    vector<string>::iterator iter;
+    string word;
+
+    while(cin >> word){
+
+        word = desaxUTF8(word);         //transforms 'ą' to 'a', 'ż' to 'z', etc.
+        word = removePunctuation(word); //removes non-letters
+        word = toLowerCase(word);       //changes every letter to lower case
+
+        if(word.length() > 1)
+            text.push_back(word);
+
+    }
+
+
+    for(int i = 0; i < text.size(); ++i){
+        cout << text.at(i) << "\n";
+    }
+
+    return 0;
+}
