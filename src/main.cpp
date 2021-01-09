31d79b38fff321b6edf2055b024199b32d523012
areckt <arkadiusz.dawid.99@gmail.com>
2021-01-09 14:11:38 +0100

    pobieranie argumentow i wyswietlanie pomocy

--- src/main.cpp
+++ src/main.cpp
@@ -12,29 +12,32 @@
 #include <locale>
 #include <algorithm>
 #include "../include/generator.h"
+#include "../include/benchmark.h"
 
 using namespace std;
 
 #define PRIME 7
 
-unsigned long int hashFunction(wstring s){  // calculates hash, returns k (before modulo TableSize)
-    int g = 31;
-    unsigned long int k = 0;
-    for(char i : s){
-        k = g * k + (int)i;
-    }
-
-    return k;
-}
+unsigned long int hashFunction(wstring s);
+void displayHelp();
 
 int main(int argc, char *argv[]){
 
-    /*
      if(argc > 1){
-        const int tableSize = stoi(argv[1], nullptr, 10);
-        cout << "Table size:" << tableSize << endl;
-    }
-    */
+         string arg1 = argv[1];
+         if(arg1[0] == 'h'){
+             displayHelp();
+             return 0;
+         }
+         //const int tableSize = stoi(argv[1], nullptr, 10);
+         for(int i = 0; i < argc; ++i) {
+             cout << "Argument " << i << "\t" << argv[i] << endl;
+        }
+     }
+     else{
+         cout << "Error: no parameters given\nIf you need help, try ./prog h" << endl;
+         return -1;
+     }
     
     wifstream file("../util/out.txt");
     file.imbue(std::locale("C.UTF-8"));
@@ -57,7 +60,6 @@ int main(int argc, char *argv[]){
     vector<vector<int>> second_letter_dist = calcSecondDist(text);
     vector<int> wordLength = calcWordLengthDist(text);
 
-
     int TableSize = 11;
 
     vector<wstring> hashTable (TableSize);
@@ -65,13 +67,6 @@ int main(int argc, char *argv[]){
     unsigned long int k;
     unsigned int k1;
 
-    /*
-    // initialize vectors of probability for generator
-    vector<float> startProbability (26, 0); // prob of first letter in word
-    vector<vector<float>> nextProbability (26, vector<float>(27, 0));   // prob of letter after other letter
-    */
-    
-    
 
     // main loop - inserting to the hashTable
     for(int i = 0; i < TableSize; i++){
@@ -104,4 +99,30 @@ int main(int argc, char *argv[]){
     }
 
     return 0;
+}
+
+unsigned long int hashFunction(wstring s){  // calculates hash, returns k (before modulo TableSize)
+    int g = 31;
+    unsigned long int k = 0;
+    for(char i : s){
+        k = g * k + (int)i;
+    }
+
+    return k;
+}
+
+void displayHelp(){
+    cout << "Available run modes:\n\n";
+
+    cout << "1) ./prog t1 tableSize\n";
+    cout << "It allows you to manually type in words for hashTable.\ntableSize should be a small integer.\n";
+    cout << "You can type in '0' to stop. At the end, content of hashTable will be displayed\n\n";
+
+    cout << "2) ./prog t2 tableSize in.txt out.txt\n";
+    cout << "It will generate random words based on a text from in.txt and put them in hashTable.\n";
+    cout << "The time measurement will be saved to out.txt.\n";
+    cout << "tableSize should be an integer from the range 1 - 2147483647\n\n";
+
+    cout << "3) ./prog h\n";
+    cout << "Show this help message.\n";
 }
