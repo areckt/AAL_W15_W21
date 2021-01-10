d2b81424955faffe8507ff8452a669db7e08f9e3
areckt <arkadiusz.dawid.99@gmail.com>
2021-01-10 16:04:20 +0100

    Lepszy tryb 2 (powtórzenia), pomiary

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <locale>
 #include <algorithm>
+#include <iomanip>
 #include "../include/generator.h"
 #include "../include/benchmark.h"
 
@@ -26,9 +27,9 @@ void printHashTable(vector<wstring> &hashTable);
 int main(int argc, char *argv[]){
 
     string mode, inputFile, outputFile;
-    int tableSize;
+    int tableSize, repetitions;
 
-    if(argc == 2 || argc == 3 || argc == 5){
+    if(argc == 2 || argc == 3 || argc == 6){
          mode = argv[1];
          if(mode[0] == 'h'){
              displayHelp();
@@ -36,14 +37,11 @@ int main(int argc, char *argv[]){
          }
 
          tableSize = stoi(argv[2], nullptr, 10);
-         if(argc == 5){
+         if(argc == 6){
              inputFile = argv[3];
              outputFile = argv[4];
+             repetitions = stoi(argv[5], nullptr, 10);
          }
-
-         for(int i = 0; i < argc; ++i) {
-             cout << "Argument " << i << "\t" << argv[i] << endl;
-        }
      }
     else{
          cout << "Error: invalid number of parameters given\nIf you need help, try ./prog h" << endl;
@@ -61,7 +59,6 @@ int main(int argc, char *argv[]){
      */
 
     if(mode == "t1"){
-
         for(int i = 0; i < tableSize; ++i){
             cout << "Type in word: ";
             wcin >> s;
@@ -69,13 +66,10 @@ int main(int argc, char *argv[]){
             if(s.at(0) == '0'){
                 break;
             }
-
             putIntoHashTable(hashTable, s, tableSize, k, k1);
         }
-
         printHashTable(hashTable);
         return 0;
-
     }
 
 
@@ -84,37 +78,58 @@ int main(int argc, char *argv[]){
      */
 
     if(mode == "t2"){
-        wifstream file(inputFile);
-        file.imbue(std::locale("C.UTF-8"));
-        if(!file.good())
-        {
-            std::cout << "Cannot open file" << endl;
+        wifstream fileIn(inputFile);
+        fileIn.imbue(std::locale("C.UTF-8"));
+        if(!fileIn.good()){
+            std::cout << "Cannot open inputFile" << endl;
             return -1;
         }
-
         vector<wstring> text;
         wstring word;
 
-        while(!file.eof())
-        {
-            file >> word;
+        while(!fileIn.eof()){
+            fileIn >> word;
             text.push_back(word);
         }
-
+        fileIn.close();
         vector<int> first_letter_dist = calcStartDist(text);
         vector<vector<int>> second_letter_dist = calcSecondDist(text);
         vector<int> wordLength = calcWordLengthDist(text);
 
-        for(int i = 0; i < tableSize; i++){
-            s = generateWord(first_letter_dist, second_letter_dist, wordLength);
-            putIntoHashTable(hashTable, s, tableSize, k, k1);
+        ofstream fileOut;
+        fileOut.open(outputFile, ios::out | ios::ate | ios::app);
+        if(!fileOut.good()){
+            std::cout << "Cannot open outputFile" << endl;
+            return -1;
         }
+        fileOut << "Measurement parameters:\ttableSize: " << tableSize << "\tRepetitions: " << repetitions << "\n";
 
+        size_t elapsedNano;
+        float elapsedMilli, avgElapsedMilli = 0;
+
+        for(int r = 0; r < repetitions; ++r) {
+
+            hashTable = vector<wstring>(tableSize); // resets hashTable
+            elapsedNano = 0;
+
+            for (int i = 0; i < tableSize; i++) {
+                s = generateWord(first_letter_dist, second_letter_dist, wordLength);
+                Benchmark<std::chrono::nanoseconds> b;  // Start measuring time
+                putIntoHashTable(hashTable, s, tableSize, k, k1);
+                elapsedNano += b.elapsed();   // Stop measuring time
+            }
+
+            elapsedMilli = (float)(elapsedNano)/1000000;
+            avgElapsedMilli += elapsedMilli;
+            fileOut << "Milliseconds:\t" << setprecision(2) << fixed << elapsedMilli << "\t\tNanoseconds:\t" << elapsedNano << "\n";
+
+        }
+        avgElapsedMilli /= (float)repetitions;
+        fileOut << "Average time in ms:\t" << avgElapsedMilli << "\n\n";
+        fileOut.close();
         printHashTable(hashTable);
         return 0;
-
     }
-
 
     return 0;
 }
@@ -133,13 +148,13 @@ void displayHelp(){
     cout << "Available run modes:\n\n";
 
     cout << "1) ./prog t1 tableSize\n";
-    cout << "It allows you to manually type in words for hashTable.\ntableSize should be a small integer.\n";
+    cout << "It allows you to manually type in words for hashTable.\ntableSize should be a small prime number.\n";
     cout << "You can type in '0' to stop. At the end, content of hashTable will be displayed\n\n";
 
-    cout << "2) ./prog t2 tableSize in.txt out.txt\n";
+    cout << "2) ./prog t2 tableSize in.txt out.txt repetitions\n";
     cout << "It will generate random words based on a text from in.txt and put them in hashTable.\n";
-    cout << "The time measurement will be saved to out.txt.\n";
-    cout << "tableSize should be an integer from the range 1 - 2147483647\n\n";
+    cout << "The time measurements will be done repetitions-times and saved to out.txt.\n";
+    cout << "tableSize should be a prime number from the range 1 - 2147483647\n\n";
 
     cout << "3) ./prog h\n";
     cout << "Show this help message.\n";
